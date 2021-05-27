4d84b7b2946d38fb2d3f5a3de60411777f3de3d1
zaid <zaidlafi52@gmail.com>
2021-05-27 09:30:08 +0300

    init

---
+++ src/main/java/automation_start/demo/App.java
@@ -0,0 +1,13 @@
+package automation_start.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
